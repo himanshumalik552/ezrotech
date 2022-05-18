0aedb99ee69841925076306644a39acdf050f70a
Himanshu <himanshu.himanshu@rhdhv.com>
2022-05-18 14:21:37 +0530

    set mobile footer test

--- src/pages/Footer/FooterCommon.js
+++ src/pages/Footer/FooterCommon.js
@@ -6,9 +6,31 @@ const FooterCommon = () => {
   const googleplus  ="https://myaccount.google.com/profile?pli=1";
   const facebook  ="https://www.facebook.com";
   const instagram  ="https://www.instagram.com";
-  let whatsapp  ="";
-  let twitter  ="";
-
+  const whatsapp  ="";
+  const twitter  ="";
+  const data = [{
+    className:"facebook",
+    link:facebook,
+    icon:"fa fa-facebook fa-2x",
+  },
+  {
+    className:"twitter",
+    link:twitter,
+    icon:"fa fa-twitter fa-2x",
+  },{
+    className:"instagram",
+    link:instagram,
+    icon:"fa fa-instagram fa-2x",
+  },{
+    className:"google",
+    link:googleplus,
+    icon:"fa fa-google fa-2x",
+  },{
+    className:"whatsapp",
+    link:whatsapp,
+    icon:"fa fa-whatsapp fa-2x",
+  },
+]
   return (
     <div>
       <section
@@ -31,11 +53,10 @@ const FooterCommon = () => {
             
 <div className="wrapper_footer">
   <ul  className=' row justify-content-center  '>
-    <li className="facebook"><Link href={facebook}><i className="fa fa-facebook fa-2x" aria-hidden="true"></i></Link></li>
-    <li className="twitter"><Link href={twitter}><i className="fa fa-twitter fa-2x" aria-hidden="true"></i></Link></li>
-    <li className="instagram"><Link href={instagram}><i className="fa fa-instagram fa-2x" aria-hidden="true"></i></Link></li>
-    <li className="google"><Link href={googleplus}><i className="fa fa-google fa-2x" aria-hidden="true"></i></Link></li>
-    <li className="whatsapp"><Link href={whatsapp}><i className="fa fa-whatsapp fa-2x" aria-hidden="true"></i></Link></li>
+  {data.map((item)=>     
+      <li key={item.link} className={item.className}><a  rel="noreferrer"  target="_blank" href={item.link}><span><i className={item.icon}  aria-hidden="true"></i></span></a></li>      
+    )
+    }
   </ul>
 </div>
    
